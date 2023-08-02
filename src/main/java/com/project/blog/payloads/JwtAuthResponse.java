df98129fb540476092bc3d993bb309698156b228
Hariom Sharma <hariomne@gmail.com>
2023-08-02 12:20:59 +0530

    bugs fixes.

--- src/main/java/com/project/blog/payloads/JwtAuthResponse.java
+++ src/main/java/com/project/blog/payloads/JwtAuthResponse.java
@@ -1,11 +1,29 @@
 package com.project.blog.payloads;
 
-import lombok.Data;
-
-@Data
 public class JwtAuthResponse {
 
 	private String token;
-	
 	private UserDto user;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public UserDto getUser() {
+		return user;
+	}
+
+	public void setUser(UserDto user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "JwtAuthResponse [token=" + token + ", user=" + user + "]";
+	}
+
 }
