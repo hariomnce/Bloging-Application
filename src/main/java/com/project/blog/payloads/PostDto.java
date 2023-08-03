0b9f994b07835baf26c380f8b9e1b086df09f752
Hariom Sharma <hariomne@gmail.com>
2023-08-03 10:58:28 +0530

    bugs fixes.

--- src/main/java/com/project/blog/payloads/PostDto.java
+++ src/main/java/com/project/blog/payloads/PostDto.java
@@ -4,42 +4,89 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-import com.project.blog.entities.Category;
-import com.project.blog.entities.Comment;
-import com.project.blog.entities.User;
-
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 public class PostDto {
 
 	private Integer postId;
-	
 	private String title;
-	
 	private String content;
-	
 	private String imageName;
-	
-	private Date addedDate;	
-	
+	private Date addedDate;
 	private CategoryDto category;
-
 	private UserDto user;
-	
-	private Set<CommentDto> comments=new HashSet<>();
+	private Set<CommentDto> comments = new HashSet<>();
 
-	
-	
-	
-	
-	
-	
+	public Integer getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Integer postId) {
+		this.postId = postId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public CategoryDto getCategory() {
+		return category;
+	}
+
+	public void setCategory(CategoryDto category) {
+		this.category = category;
+	}
+
+	public UserDto getUser() {
+		return user;
+	}
+
+	public void setUser(UserDto user) {
+		this.user = user;
+	}
+
+	public Set<CommentDto> getComments() {
+		return comments;
+	}
+
+	public void setComments(Set<CommentDto> comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public String toString() {
+		return "PostDto [postId=" + postId + ", title=" + title + ", content=" + content + ", imageName=" + imageName
+				+ ", addedDate=" + addedDate + ", category=" + category + ", user=" + user + ", comments=" + comments
+				+ "]";
+	}
+
 }
