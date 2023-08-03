5d5cf2aa8acba8f9d57642bc518654eb50dbb11d
Hariom Sharma <hariomne@gmail.com>
2023-08-03 11:18:00 +0530

    bugs fixes.

--- src/main/java/com/project/blog/entities/Category.java
+++ src/main/java/com/project/blog/entities/Category.java
@@ -13,29 +13,62 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Table(name="categories")
+@Table(name = "categories")
 @NoArgsConstructor
-@Getter
-@Setter
 public class Category {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer categoryId;
-	
-	@Column(name="title",length = 100,nullable = false)
+
+	@Column(name = "title", length = 100, nullable = false)
 	private String categoryTitle;
-	
-	@Column(name="description")
+
+	@Column(name = "description")
 	private String categoryDescription;
-	
-	
-	@OneToMany(mappedBy = "category",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-	private List<Post> posts=new ArrayList<>();
-	
+
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryTitle() {
+		return categoryTitle;
+	}
+
+	public void setCategoryTitle(String categoryTitle) {
+		this.categoryTitle = categoryTitle;
+	}
+
+	public String getCategoryDescription() {
+		return categoryDescription;
+	}
+
+	public void setCategoryDescription(String categoryDescription) {
+		this.categoryDescription = categoryDescription;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [categoryId=" + categoryId + ", categoryTitle=" + categoryTitle + ", categoryDescription="
+				+ categoryDescription + ", posts=" + posts + "]";
+	}
+
 }
